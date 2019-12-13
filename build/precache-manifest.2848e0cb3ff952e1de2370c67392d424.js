92ec670890f0df1972fcc55125c87d91e98b85b8
Joy Tempkins <jtempkins@gmail.com>
2019-12-12 21:55:44 -0600

    gitHub Pages deployed

---
+++ build/precache-manifest.2848e0cb3ff952e1de2370c67392d424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b8b5c9f2e4788344d9a520e0b1310c",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "276a436075de1e4c5e38",
+    "url": "/ClickyGame/static/css/main.d8b7ae8f.chunk.css"
+  },
+  {
+    "revision": "eec21a1e2de20c955053",
+    "url": "/ClickyGame/static/js/2.f4bc2046.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.f4bc2046.chunk.js.LICENSE"
+  },
+  {
+    "revision": "276a436075de1e4c5e38",
+    "url": "/ClickyGame/static/js/main.f5404187.chunk.js"
+  },
+  {
+    "revision": "406c06b4e71508ca25b2",
+    "url": "/ClickyGame/static/js/runtime-main.59387589.js"
+  }
+]);
